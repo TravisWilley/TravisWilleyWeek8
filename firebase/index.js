3fbd1442eb86c2570b39abb2b1147d843770a12a
TravisWilley <97533243+TravisWilley@users.noreply.github.com>
2023-10-23 01:36:21 +0000

    Initial commit

---
+++ firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// replace this firebase conFigvariable with your own
+const firebaseConfig = {
+  apiKey: 'AIzaSyAmozceDBatGvrM5sMR3U_kIrFtwUaMAL0',
+  authDomain: 'week7-6900a.firebaseapp.com',
+  projectId: 'week7-6900a',
+  storageBucket: 'week7-6900a.appspot.com',
+  messagingSenderId: '582849656351',
+  appId: '1:582849656351:web:da1cc96f8a0b0d3d98872a',
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+export { auth, db };
